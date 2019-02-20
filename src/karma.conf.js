ac5090507cfcc00fb9744fdfbefeecddc33d6ff2
Tim Williamson <timwilliamson1337@gmail.com>
2019-02-20 22:16:00 +0000

    update tests with mocha+chai, import angular materials

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,11 +4,11 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['mocha', 'chai', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
+      require('karma-mocha'),
+      require('karma-chai'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
