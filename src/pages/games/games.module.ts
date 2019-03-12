eaf3cdb06a6df29c5ee79f309095061e036258f2
Tim Williamson <timothy.williamson@santander.co.uk>
2019-03-12 17:22:45 +0000

    init landing and games page modules

---
+++ src/pages/games/games.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GamesComponent } from './components/games/games.component';
+
+@NgModule({
+  declarations: [GamesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GamesModule { }
