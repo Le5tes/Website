621da661ffcec7f8cb2583a9cc6bcb6b99de3fad
Tim Williamson <timothy.williamson@santander.co.uk>
2019-03-22 17:06:24 +0000

    create slide-selector component

---
+++ src/modules/slide-selector/components/slide-selector/slide-selector.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-selector',
+  templateUrl: './slide-selector.component.html',
+  styleUrls: ['./slide-selector.component.scss']
+})
+export class SlideSelectorComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
---
+++ src/modules/slide-selector/slide-selector.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SlideSelectorComponent } from './components/slide-selector/slide-selector.component';
+
+@NgModule({
+  declarations: [SlideSelectorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SlideSelectorModule { }
