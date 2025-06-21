5206cb97e4b7537ed3252b26e417c64bacbf21f0
Tim Williamson <tim.williamson@kaluza.com>
2025-06-21 16:21:53 +0100

    make urls https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  blogUrl: "http://api.lestes.tech",
+  blogUrl: "https://api.lestes.tech",
   projects: {
-    sudokuUrl: "http://sudoku.lestes.tech"
+    sudokuUrl: "https://sudoku.lestes.tech"
   }
 };
